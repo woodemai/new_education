585b591eef4feff525c46579820f7848d1a613de
woodemai <n.savchenkoo73@gmail.com>
2023-05-26 15:33:48 +0300

    change layout istall prisma

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
     return (
         <footer>
-
+            <nav>
+                <span>Contact us - <a href="mailto: n.savchenkoo73@gmail.com">n.savchenkoo73@gmail.com</a></span>
+                <span>new education 2023 - All rights reserved</span>
+            </nav>
         </footer>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <header>
             <nav>
-
+                <h1>new education</h1>
+                <ul>
+                    <li><Link href={'/'}>Home</Link></li>
+                    <li><Link href={'/courses'}>Courses</Link></li>
+                    <li><Link href={'/courses'}>Profile</Link></li>
+                    <li><Link href={'/courses'}>Settings</Link></li>
+                    <li><Link href={'/courses'}>Quit</Link></li>
+                </ul>
             </nav>
         </header>
     )
