d4365938284be3c8103186912acf2e68af8a80c9
woodemai <n.savchenkoo73@gmail.com>
2023-05-28 01:15:03 +0300

    adding auth

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,8 @@
+'use client'
+import {useSession} from "next-auth/react";
+
 export default async function Profile() {
+    const session = useSession()
+    console.log(session)
     return (<>Profile</>);
 }
