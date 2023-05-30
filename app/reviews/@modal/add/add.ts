eccc18ee53902d0d570dba516a43f7df40495db9
woodemai <n.savchenkoo73@gmail.com>
2023-05-31 00:55:52 +0300

    adding review adding

--- app/reviews/@modal/add/add.ts
+++ app/reviews/@modal/add/add.ts
@@ -4,6 +4,8 @@ import {ReviewProps} from "@/lib/interfaces";
 
 export default async function addReview(review: ReviewProps) {
     const {title, body, userId} = review;
+    console.log(review)
+    console.log(userId)
     await prisma.review.create({
         data: {
             title,
