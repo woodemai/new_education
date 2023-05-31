0e38c359a1631f040d564d8224e375556c27c69c
woodemai <n.savchenkoo73@gmail.com>
2023-05-31 19:47:40 +0300

    change profile and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import List from "@/components/List";
 import Item from "@/components/Item";
 import ReactMarkdown from "react-markdown";
-const renderItem = (page: {name:string, description: string, href: string}) => {
+
+const renderItem = (page: { name: string, description: string, href: string }) => {
     return (
         <Item name={page.name} description={page.description} href={page.href}/>
     )
@@ -29,15 +30,31 @@ export default function Home() {
             href: '/settings'
         },
     ]
+    const tasks = [
+        {
+            name: "GitHub",
+            description: "Add GitHub provider to NextAuth",
+            href: '/'
+        },
+        {
+            name: "Lessons",
+            description: "Implement ability to add to lessons links, images, videos and other content",
+            href: '/'
+        },
+        {
+            name: "Roles",
+            description: "Add roles: Admin, Student, Teacher. Admin and teacher can add lessons, admin also can add courses.",
+            href: '/'
+        },
+    ]
     return (
         <>
             <h1>Modern Fast Secure</h1>
             <ReactMarkdown>
                 New Education is a website for online education. There are a lot of courses on various topics.
             </ReactMarkdown>
-            <List items={pages}
-                  element={(page) => renderItem(page)}
-                  heading={"Pages"}/>
+            <List items={pages} element={(page) => renderItem(page)} heading={"Pages"}/>
+            <List items={tasks} element={(task) => renderItem(task)} heading={'Roadmap'}/>
         </>
     )
 }
