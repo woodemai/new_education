cf1814518e9b3230059afd14c1677e04d225307f
woodemai <n.savchenkoo73@gmail.com>
2023-06-03 20:14:37 +0300

    adding GitHub provider to NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import YandexProvider from "next-auth/providers/yandex";
+import GitHubProvider from "next-auth/providers/github";
 import NextAuth from "next-auth";
 
 const handler = NextAuth({
@@ -9,6 +10,10 @@ const handler = NextAuth({
         YandexProvider({
             clientId: String(process.env.YANDEX_CLIENT_ID),
             clientSecret: String(process.env.YANDEX_CLIENT_SECRET),
+        }),
+        GitHubProvider({
+            clientId: String(process.env.GITHUB_ID),
+            clientSecret: String(process.env.GITHUB_SECRET),
         })
     ],
 })
