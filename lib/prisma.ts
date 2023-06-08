3ce99e970a563f896fc5ad89df06caf2f03a9180
woodemai <n.savchenkoo73@gmail.com>
2023-06-08 03:07:42 +0300

    adding translation to russian 11 [bug fix] move page api -> app api
    Signed-off-by: woodemai <n.savchenkoo73@gmail.com>

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,11 @@ import {PrismaClient} from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
+const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         log: ["query"],
     });
 
 if (process.env.NODE_ENV != "production") globalForPrisma.prisma;
+export default prisma;
