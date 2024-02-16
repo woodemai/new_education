58b1821673271fbb0d183301c57b3595219b20c3
woodemai <n.savchenkoo73@gmail.com>
2024-02-17 02:16:10 +0300

    merge main

--- app/[lang]/reviews/layout.tsx
+++ app/[lang]/reviews/layout.tsx
@@ -4,28 +4,30 @@ import { getDictionary } from "@/get-dictionaries";
 import Header from "@/components/pages/review/header/Header";
 
 interface Props {
-import { ReactNode } from "react";
-import { Locale } from "@/i18n-config";
-import { getDictionary } from "@/get-dictionaries";
-import Header from "@/components/pages/review/header/Header";
-
-interface Props {
     children: ReactNode,
     params: { lang: Locale }
 }
 
+<<<<<<< HEAD
 const Layout = async ({ children, params }: Props) => {
 
     const { reviewsPage } = await getDictionary(params.lang);
+=======
+const Layout = async ({ children, modal, params: { lang } }: Props) => {
+
+    const { reviewsPage } = await getDictionary(lang);
+>>>>>>> main
 
     return (
         <>
             <Header dictionary={reviewsPage.header} />
+<<<<<<< HEAD
+=======
+            {modal}
+>>>>>>> main
             {children}
         </>
     )
 }
-
-export default Layout
 
 export default Layout
